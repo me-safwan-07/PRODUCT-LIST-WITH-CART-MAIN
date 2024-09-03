5304e83fa4e21975e031d56cd5ca31bcb54eaeb8
me-safwan-07 <mesafwan07@gmail.com>
2024-09-03 21:03:11 +0530

    add the add cart button

--- src/components/Desserts.tsx
+++ src/components/Desserts.tsx
@@ -1,44 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from '../../data.json';
+import { MdAddShoppingCart } from "react-icons/md";
 
 const Desserts:React.FC = () => {
-    
+    const [cartCount, setCartCount] = useState(1)
+
+    const handleCartCountAdd = () => {
+        setCartCount(cartCount + 1);
+    }
+    const handleCartCountLess = () => {
+        if (cartCount > 0) {
+            setCartCount(cartCount - 1);
+        }
+        
+    }
   return (
     <>
-        <div className="m-12 flex">
+        <div className="m-0 px-36 py-12 bg-pink-100 flex">
             <div className="w-3/4">
-                <h2>Deserts</h2>
-                <div className="grid grid-cols-3 grid-rows-3 gap-4">
+                <h1 className='text-4xl py-10 font-bold'>Desserts</h1>
+                <div className="flex flex-row flex-wrap gap-4">
                     {data.map((cake, index) => (
-                        <div key={index} className="m-1 relative">
-                            <img 
-                                src={cake.image.desktop}
-                                className="rounded-lg p-1 w-full h-auto"
-                                alt={cake.name}
-                            />
+                        <div key={index} className="m-1 w-1/4 h-auto relative rounded-sm">
+                        <img 
+                            src={cake.image.desktop}
+                            className="rounded-lg p-1 w-full h-auto"
+                            alt={cake.name}
+                        />
+                        {cartCount == 0 ? 
                             <button
-                                className='border rounded-full absolute bottom-20 right-20 z-50 px-4 py-2 bg-white' /* Adjusted positioning */
-                            >
-                                Add To Cart
-                            </button>
-                            <div className="pt-6">
-                                <div className="">
-                                    <p key={index}>{cake.category}</p>
-                                    <p key={index}>{cake.name}</p>
-                                </div>
-                                <p key={index}>${cake.price.toFixed(2)}</p>
+                                className='border rounded-full absolute z-10 px-3 py-2 bg-white text-sm flex gap-2'
+                                style={{
+                                    top: '50%',
+                                    left: '50%',
+                                    width: '120px', // Adjust width as needed
+                                    height: '45px', // Adjust height as needed
+                                    transform: 'translate(-50%, -50%)', // Center the button
+                                    marginTop: '30px', // Adjust to move half of the button inside the image
+                                    marginLeft: '0px', // Adjust to move half of the button inside the image
+                                }}
+                                onClick={handleCartCountAdd}
+                            > 
+                                <MdAddShoppingCart />Add To Cart
+                            </button> :
+                            <div className="">
+                                <button onClick={handleCartCountLess}>-</button> {cartCount} <button onClick={handleCartCountAdd}>+</button>
                             </div>
+                        
+                        }
+                        
+                        <div className="pt-6">
+                            <div>
+                                <p key={index} className='gray'>{cake.category}</p>
+                                <p key={index}>{cake.name}</p>
+                            </div>
+                            <p key={index}>${cake.price.toFixed(2)}</p>
                         </div>
+                    </div>
+                    
                     ))}
                 </div>
             </div>
             
-            <div className="w-1/4 border">
+            <div className="w-1/4 m-14 h-20 bg-white">
                 <h2>Order Now</h2>
                 <div className=""></div>
                 <div className=""></div>
                 <div className="">
-                    <button>Add To cart</button>
+                    <button
+                        className='rounded-full border px-14 py-4 bg-orange-500'
+                    >Confirm Order</button>
                 </div>
             </div>
         </div>
